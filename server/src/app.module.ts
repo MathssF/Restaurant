5990c3bee54c35f66178c29ffa62a9a1638fa0e0
MathssF <102183898+MathssF@users.noreply.github.com>
2026-02-16 20:53:13 +0000

    nest

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,10 +1,16 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AdminModule } from './admin/admin.module';
+import { ClientModule } from './client/client.module';
+import { UserModule } from './user/user.module';
+import { CuserController } from './cuser/cuser.controller';
+
+
 
 @Module({
-  imports: [],
-  controllers: [AppController],
+  imports: [ClientModule, UserModule],
+  controllers: [AppController, CuserController],
   providers: [AppService],
 })
 export class AppModule {}
